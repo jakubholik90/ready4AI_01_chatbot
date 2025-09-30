738a2bbaaf2c8a812ca3537641deb67fded1d9e4
Jakub Holik <jakubholik90@gmail.com>
2025-10-01 00:27:32 +0200

    working chatbot with simple memory function

--- src/main/java/pl/jakubholik90/APIConnector.java
+++ src/main/java/pl/jakubholik90/APIConnector.java
@@ -1,8 +1,38 @@
 package pl.jakubholik90;
 
+import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.ChatModel;
+import com.openai.models.responses.Response;
+import com.openai.models.responses.ResponseCreateParams;
+
+import java.util.List;
 
 public class APIConnector {
 
-    OpenAIOkHttpClient.builder()
+    public static List<String> sendAndGetResponse(String input) {
+        // tworzenie klienta
+        OpenAIClient client = OpenAIOkHttpClient.builder()
+                .apiKey(PrivateData.apiKey)
+                .build();
+
+        // tworzenie parametrow
+        ResponseCreateParams params = ResponseCreateParams.builder()
+                .model(ChatModel.O3_MINI)
+                .input(input)
+                .build();
+        
+        // tworzenie zapytania
+        Response response = client.responses().create(params);
+
+        List<String> outputList = response.output().stream()
+                .flatMap(item -> item.message().stream())
+                .flatMap(item -> item.content().stream())
+                .flatMap(item -> item.outputText().stream())
+                .map(item -> item.text()).toList();
+
+        return outputList;
+    }
+
+
 }
--- src/main/java/pl/jakubholik90/Main.java
+++ src/main/java/pl/jakubholik90/Main.java
@@ -1,7 +1,45 @@
 package pl.jakubholik90;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scaner = new Scanner(System.in);
+        String welcomeMessage = "Witaj w Chatbocie!";
+        String menu = "--- Wpisz swoje zapytanie do chatbota lub podaj \"quit\" w celu zakonczenia programu";
+        String line = "---";
+        String goodbyeMessage = "Zakonczono prace Chatbota. Dziekujemy za skorzystanie";
+        String systemMessage = "--- Wysylam zapytanie ...";
+        boolean runApp = true;
+        List<String> chatHistory = new LinkedList<>();
+
+        System.out.println(welcomeMessage);
+        while (runApp) {
+            System.out.println(line);
+            System.out.println(menu);
+            System.out.println(line);
+            String input = scaner.nextLine();
+
+            if (!input.equals("quit")) {
+                chatHistory.add(input);
+                String chatHistoryString = chatHistory.toString();
+                //System.out.println("chatHistoryString:" + chatHistoryString);
+                System.out.println(systemMessage);
+                System.out.println(line);
+                List<String> output = APIConnector.sendAndGetResponse(chatHistoryString);
+                if(output.size()>0) {
+                    output.stream()
+                            .forEach(System.out::println);
+                }
+                chatHistory.add(output.toString());
+            } else {
+                System.out.println(line);
+                System.out.println(goodbyeMessage);
+                runApp = false;
+                break;
+            }
+        }
     }
 }
